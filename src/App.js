dfad8b79c43ab00387b2eee00d07183f6cab9f38
Lechusin <122849236+Lechusin@users.noreply.github.com>
2024-01-20 20:48:50 -0500

    conec backend terminado

--- src/App.js
+++ src/App.js
@@ -1,60 +1,26 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import LocalImageUploader from './components/LocalImageUploader';
-import UrlImageUploader from './components/UrlImageUploader';
-import CameraImageCapture from './components/CameraImageCapture';
+import InputMethods from './components/InputMethods';
+import Result from './components/Result';
 
 function App() {
   const [imageData, setImageData] = useState(null);
-  const [activeTab, setActiveTab] = useState('local');
 
   const handleImageChange = (data) => {
     setImageData(data);
   };
 
-  const switchTab = (tab) => {
-    setActiveTab(tab);
-  };
-
   return (
     <div className="container mt-5">
-      <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <button className={`nav-link ${activeTab === 'local' ? 'active' : ''}`} onClick={() => switchTab('local')}>
-            Desde Equipo
-          </button>
-        </li>
-        <li className="nav-item">
-          <button className={`nav-link ${activeTab === 'url' ? 'active' : ''}`} onClick={() => switchTab('url')}>
-            Desde URL
-          </button>
-        </li>
-        <li className="nav-item">
-          <button className={`nav-link ${activeTab === 'camera' ? 'active' : ''}`} onClick={() => switchTab('camera')}>
-            Con Cámara
-          </button>
-        </li>
-      </ul>
-
-      <div className="tab-content">
-        <div className={`tab-pane ${activeTab === 'local' ? 'active' : ''}`} id="local">
-          <LocalImageUploader onImageChange={handleImageChange} />
+      <div className="row">
+        <div className="col-md-6">
+          <InputMethods onImageChange={handleImageChange} />
         </div>
-        <div className={`tab-pane ${activeTab === 'url' ? 'active' : ''}`} id="url">
-          <UrlImageUploader onImageChange={handleImageChange} />
-        </div>
-        <div className={`tab-pane ${activeTab === 'camera' ? 'active' : ''}`} id="camera">
-          <CameraImageCapture onImageChange={handleImageChange} />
+        <div className="col-md-6">
+          <Result imageData={imageData} />
         </div>
       </div>
-
-      {imageData && (
-        <div className="mt-5">
-          <h2>Imagen Capturada o Cargada</h2>
-          <img src={imageData} alt="Imagen" className="img-fluid" />
-        </div>
-      )}
     </div>
   );
 }
